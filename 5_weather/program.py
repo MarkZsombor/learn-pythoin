648040e398d4c5ab6bb569a515271d45ee350522
Mark Zsombor <markszsombor@gmail.com>
2018-05-17 12:32:03 -0700

    prgram is retrieving html from web

--- 5_weather/program.py
+++ 5_weather/program.py
@@ -1,9 +1,22 @@
+import requests
+
 def main():
-  # get zipcode
-  # get html
+  print_header()
+  area_code = input('What zipcode do you want the weather for?')
+  html = get_html_from_web(area_code)
   # parse html
   # display forecast
-  print('main')
+
+
+def print_header():
+  print('Weather App')
+
+def get_html_from_web(zip_code):
+  url = 'http://www.wunderground.com/weather/{}'.format(zip_code)
+  response = requests.get(url)
+  # print(url)
+  # print(response.text[0:250])
+  return response.text
 
 if __name__ == '__main__':
   main()
